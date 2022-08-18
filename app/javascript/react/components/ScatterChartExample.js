f59bf354e8c3ba364a858ac7cb413c92b2df25f9
dovakeen118 <kegillis118@gmail.com>
2022-08-18 12:45:30 -0400

    update starter code with event example

--- app/javascript/react/components/ScatterChartExample.js
+++ app/javascript/react/components/ScatterChartExample.js
@@ -8,21 +8,41 @@ const ScatterChartExample = (props) => {
         <h2> Below is an example Scatter Chart</h2>
         <Chart
           chartType="ScatterChart"
-          data={[
-            ['Cohort Number', 'Awesomeness', 'Stress Levels', 'Sleep Deprivation', 'Caffeine Intake'],
+          data={[['Cohort Number', 'Awesomeness', 'Stress Levels', 'Sleep Deprivation', 'Caffeine Intake'],
             ['53',  1000,      400, 250, 500],
             ['54',  1170,      460, 350, 1000],
             ['55',  660,       1120, 250, 800],
             ['56',  1201,      602, 450, 700],
             ['57',  1250,      650, 650, 600],
             ['58',  1500,      630, 850, 500],
-            ['59',  1030,      800, 1050, 1000]
+            ['59',  1030,      800, 1050, 1000]]}
+          chartEvents={[
+            {
+              eventName: 'select',
+              callback: ({ chartWrapper }) => {
+                const chart = chartWrapper.getChart()
+                const selection = chart.getSelection()
+                if (selection.length === 1) {
+                  const [selectedItem] = selection
+                  const dataTable = chartWrapper.getDataTable()
+                  const { row, column } = selectedItem
+                  console.log(selectedItem)
+                  alert(
+                    'You selected : ' +
+                      JSON.stringify({
+                        row,
+                        column,
+                        value: dataTable.getValue(row, column),
+                        columnLabel: dataTable.getColumnLabel(column)
+                      }),
+                    null,
+                    2,
+                  )
+                }
+                console.log(selection)
+              },
+            },
           ]}
-          options={{}}
-          graph_id="ScatterChart"
-          width="100%"
-          height="400px"
-          legend_toggle
         />
       </div>
     </div>
